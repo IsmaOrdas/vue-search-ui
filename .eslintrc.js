4879e3d375f312efa0b95971016a6ef63fb6de6e
Ismael Ordás <ismael.ordas@mimacom.com>
2021-03-08 20:13:12 +0100

    Add missing eslint config.

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: [
+        'plugin:vue/essential',
+        '@vue/airbnb',
+    ],
+    parserOptions: {
+        parser: 'babel-eslint',
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'import/prefer-default-export': 'off',
+        'consistent-return': 'off',
+        'prefer-destructuring': 'off',
+        'prefer-template': 'off',
+        'max-len': 'off',
+    },
+    overrides: [
+        {
+            files: [
+                '**/__tests__/*.{j,t}s?(x)',
+                '**/tests/unit/**/*.spec.{j,t}s?(x)',
+            ],
+            env: {
+                jest: true,
+            },
+        },
+    ],
+};
