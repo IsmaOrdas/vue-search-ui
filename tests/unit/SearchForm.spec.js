e80ae3ef216667da663cc1851a8d6055973eb181
Ismael Ordás <ismael.ordas@mimacom.com>
2021-04-26 11:51:34 +0200

    Fix unit tests

--- tests/unit/SearchForm.spec.js
+++ tests/unit/SearchForm.spec.js
@@ -4,12 +4,12 @@ import { mount } from '@vue/test-utils';
 describe('SearchForm', () => {
   test('Form input should be visible', () => {
     const wrapper = mount(SearchForm);
-    expect(wrapper.find('input[type="text"]').isVisible()).toBe(true);
+    expect(wrapper.find('[name="search-input"').isVisible()).toBe(true);
   });
 
   test('Set input value', async () => {
     const wrapper = mount(SearchForm);
-    const input = wrapper.find('[data-test-id="search-input"]');
+    const input = wrapper.find('[name="search-input"');
 
     await input.setValue('Frank Ocean');
     expect(input.element.value).toBe('Frank Ocean');
@@ -24,7 +24,7 @@ describe('SearchForm', () => {
 
   test('Trigger submit form with query string', async () => {
     const wrapper = mount(SearchForm);
-    const input = wrapper.find('[data-test-id="search-input"]');
+    const input = wrapper.find('[name="search-input"');
 
     await input.setValue('Frank Ocean');
     await wrapper.find('[data-test-id="search-form"]').trigger('submit');
